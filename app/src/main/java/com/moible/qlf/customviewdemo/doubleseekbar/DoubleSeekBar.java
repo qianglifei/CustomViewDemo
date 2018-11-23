38ad3647ca4b7c6429fbea9b0633bce2deda16b2
qlf <qianglifei@gmail.com>
2018-11-23 18:02:05 +0800

    自定义ViewDoubleSeekBar

--- app/src/main/java/com/moible/qlf/customviewdemo/doubleseekbar/DoubleSeekBar.java
+++ app/src/main/java/com/moible/qlf/customviewdemo/doubleseekbar/DoubleSeekBar.java
@@ -5,6 +5,7 @@ import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.support.annotation.Nullable;
@@ -19,15 +20,19 @@ import com.moible.qlf.customviewdemo.util.DensityUtil;
 public class DoubleSeekBar extends View {
     //刻度数量
     private Integer scaleNumber = 0;
-    //绘画刻度
-    private Paint mPaintScale;
+    //绘画刻度，字体
+    private Paint mPaintScale,mPaintText;
     //将要绘制的图片
     private Bitmap seekBarSlideBlockBit,seekBarForegroundBit,seekBarBackgroundBit;
     //seekBar的起始位置,长度
     private int mSeekBarBegin = 0,mSeekBarWidth,mSeekBarTop = 0,mSeekBarBottom;
-
     //控制图片绘制的范围
     private Rect mRectBack,mRectFore,mRectLeft,mRectRight;
+    //滑块的宽度以及高度
+    private static final Integer SLIDER_WIDTH = 50;
+    private static final Integer SLIDER_HEIGHT = 100;
+    String[] scaleArray;
+    Integer[] titleDistance = new Integer[10];
     public DoubleSeekBar(Context context) {
        this(context,null);
         Log.i("TAG", "===DoubleSeekBar1: " + mSeekBarBottom);
@@ -39,8 +44,12 @@ public class DoubleSeekBar extends View {
 
     public DoubleSeekBar(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        //属性数组
         TypedArray typedArray = context.obtainStyledAttributes(attrs,
                 R.styleable.DoubleSeekBar,defStyleAttr,0);
+        //获取刻度数组
+        scaleArray = this.getResources().getStringArray(R.array.titleNameArr);
+
         int a = typedArray.getIndexCount();
         for (int i = 0; i < a ; i++) {
             int attr = typedArray.getIndex(i);
@@ -48,6 +57,7 @@ public class DoubleSeekBar extends View {
                 case R.styleable.DoubleSeekBar_scaleNumber:
                      scaleNumber = typedArray.getInteger(attr,0);
                      break;
+                 default:
             }
         }
 
@@ -56,12 +66,15 @@ public class DoubleSeekBar extends View {
 //      Paint.ANTI_ALIAS_FLAG 是使位图抗锯齿的标志
 //      Paint.DITHER_FLAG 是使位图进行有利的抖动的位掩码标志
         mPaintScale = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintText = new Paint(Paint.FILTER_BITMAP_FLAG);
+
         mPaintScale.setAntiAlias(true);
 //      Android在用画笔的时候有三种Style，分别是
 //      Paint.Style.STROKE 只绘制图形轮廓（描边）
 //      Paint.Style.FILL 只绘制图形内容
 //      Paint.Style.FILL_AND_STROKE 既绘制轮廓也绘制内容
         mPaintScale.setStyle(Paint.Style.STROKE);
+        mPaintText.setStyle(Paint.Style.FILL_AND_STROKE);
 
         //滑动块图
         seekBarSlideBlockBit = BitmapFactory.decodeResource(getResources(),R.mipmap.icon_tuodong);
@@ -74,6 +87,8 @@ public class DoubleSeekBar extends View {
         seekBarForegroundBit = BitmapFactory.decodeResource(getResources(),R.mipmap.blue);
         //进度条背景图
         seekBarBackgroundBit = BitmapFactory.decodeResource(getResources(),R.mipmap.back);
+
+
     }
 
 
@@ -107,16 +122,33 @@ public class DoubleSeekBar extends View {
         super.onDraw(canvas);
         Log.i("TAG", "===onDraw: "  + DensityUtil.px2dip(getContext(),getPaddingLeft()));
 
-        //绘制背景
-        mRectBack = new Rect(0,mSeekBarBottom / 2,getWidth(), mSeekBarBottom / 2);
+
+        for (int i = 0; i < scaleArray.length - 1 ; i++) {
+           titleDistance[i] = mSeekBarWidth / 10 * i;
+           Log.i("TAG", "===onDraw: " + titleDistance[i]);
+        }
+
+        mPaintText.setColor(Color.BLUE);
+        mPaintText.setTextSize(30);
+
+        for (int j = 0; j < titleDistance.length; j++) {
+            canvas.drawText(scaleArray[j],titleDistance[j],40,mPaintText);
+        }
+        
+//      //绘制背景
+        mRectBack = new Rect(27,60,getWidth() - 27, 70);
         canvas.drawBitmap(seekBarBackgroundBit,null,mRectBack,mPaintScale);
 
+//      //绘制前景
+        mRectFore = new Rect(27,60,getWidth() - 27, 70);
+        canvas.drawBitmap(seekBarForegroundBit,null,mRectFore,mPaintScale);
+
         //绘制左滑块
-        mRectLeft = new Rect(0,0,50,100);
+        mRectLeft = new Rect(0,50,SLIDER_WIDTH,getHeight());
         canvas.drawBitmap(seekBarSlideBlockBit,null,mRectLeft,mPaintScale);
 
         //绘制右滑块
-        mRectRight = new Rect(0,0,50,100);
+        mRectRight = new Rect(mSeekBarWidth,50,mSeekBarWidth -50,getHeight());
         canvas.drawBitmap(seekBarSlideBlockBit,null,mRectRight,mPaintScale);
     }
 }
